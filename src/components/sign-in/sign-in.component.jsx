e0eb8f27451ef2f7d03e61ae251305402fa936b2
JeongTaek Oh <jeongtaekoh@JeongTaeks-MacBook-Pro.local>
2019-08-28 01:47:48 -0700

    integrated firebase into application, created sign in page with sign in and sign out

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {signInWithGoogle} from '../../firebase/firebase.util';
+import {signInWithGoogle} from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 import { isParenthesizedExpression } from '@babel/types';
@@ -48,8 +48,13 @@ class SignIn extends React.Component{
                         label='password'
                         required />
 
-                    <CustomButton type='submit'>Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign in</CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
+                    
                 </form>
             </div>
         )
